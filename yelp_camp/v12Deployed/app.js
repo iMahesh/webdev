4574d014a3967b6208f65e98666e0adc7d4d1cec
Mahesh <reddy.mahesh001@gmail.com>
2017-03-20 01:40:03 -0500

    full-app with monoglab and heroku

---
+++ yelp_camp/v12Deployed/app.js
@@ -0,0 +1,61 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    methodOverride=require("method-override"),
+    flash       =require("connect-flash");
+    seedDB      = require("./seeds");
+    
+    
+//requring routes
+var commentRoutes    = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes      = require("./routes/index")
+    
+mongoose.connect("mongodb://localhost/yelp_camp_v12",function(err){
+// mongoose.connect(process.env.DATABASEURL,function(err){ // use it to protect the url for public. works only when you set DATABASEURL environment variable
+// mongoose.connect("mongolaburl",function(err){
+    if(err){
+        console.log(err);
+    }
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+// seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   res.locals.error=req.flash("error");
+   res.locals.success=req.flash("success")
+   next();
+});
+
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
---
+++ yelp_camp/v12Deployed/routes/comments.js
@@ -0,0 +1,84 @@
+var express = require("express");
+var router  = express.Router({mergeParams: true});
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+var middleware =require("../middleware/");
+
+//Comments New
+router.get("/new", middleware.isLoggedIn, function(req, res){
+    // find campground by id
+    console.log(req.params.id);
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("comments/new", {campground: campground});
+        }
+    })
+});
+
+//Comments Create
+router.post("/",middleware.isLoggedIn,function(req, res){
+   //lookup campground using ID
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+        Comment.create(req.body.comment, function(err, comment){
+           if(err){
+               console.log(err);
+           } else {
+               //add username and id to comment
+               comment.author.id=req.user._id;
+               comment.author.username=req.user.username;
+               comment.save();
+               campground.comments.push(comment);
+               campground.save();
+               res.redirect('/campgrounds/' + campground._id);
+           }
+        });
+       }
+   });
+});
+
+// edit comments
+router.get("/:comment_id/edit",middleware.checkCommentsOwnership,function(req,res){
+    //req.params.id this is campground id
+    console.log("req.params.id"+req.params.id);
+    Comment.findById(req.params.comment_id,function(err, foundComment) {
+        if(err){
+            res.redirect("back");
+        }else{
+            res.render("comments/edit",{campground_id:req.params.id,comment:foundComment}); 
+        }
+    });
+   
+});
+
+//update comments
+router.put("/:comment_id",middleware.checkCommentsOwnership,function(req,res){
+    console.log("the comment:"+req.body.comment);
+   Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err, updatedComment){
+      if(err){
+          res.redirect("back");
+      }else{
+           res.redirect("/campgrounds/"+req.params.id);
+      }
+   }); 
+});
+//Destroy route
+router.delete("/:comment_id",middleware.checkCommentsOwnership,function(req,res){
+   Comment.findByIdAndRemove(req.params.comment_id,function(err){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds/"+req.params.id);
+       }else{
+           req.flash("success","Comment removed successfully!");
+           res.redirect("/campgrounds/"+req.params.id);
+       }
+   });
+});
+
+
+module.exports = router;
